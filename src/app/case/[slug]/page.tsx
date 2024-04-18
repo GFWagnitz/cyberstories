d6328216e143f4fb3291a74638bfff19091502eb
Gabriel Wagnitz <gabriel.wagnitz@gmail.com>
2024-04-18 15:37:49 -0300

    add image optimization

--- src/app/case/[slug]/page.tsx
+++ src/app/case/[slug]/page.tsx
@@ -3,6 +3,7 @@ import path from 'path';
 import matter from 'gray-matter';
 import md from 'markdown-it';
 import Link from 'next/link';
+import Image from 'next/image';
 
 
 interface PostProps {
@@ -10,8 +11,7 @@ interface PostProps {
   content: string
 }
 
-async function getData(slug: string): Promise<PostProps|null>
-{
+async function getData(slug: string): Promise<PostProps | null> {
   try {
     const file = path.join(process.cwd(), 'public/posts', `${slug}.md`);
     const fileName = fs.readFileSync(file, 'utf-8');
@@ -19,31 +19,31 @@ async function getData(slug: string): Promise<PostProps|null>
     const { data: frontmatter, content } = matter(fileName);
 
     return {
-        frontmatter,
-        content
-      }; 
+      frontmatter,
+      content
+    };
 
   } catch (error) {
     console.error(error);
-    return null;  
+    return null;
   }
 };
 
 
-export default async function Page({ params } : { params: { slug: string } }) {
-  
+export default async function Page({ params }: { params: { slug: string } }) {
+
   const { frontmatter, content } = (await getData(params.slug))!;
 
   return (
     <div className="prose mx-auto mt-8">
       <Link href="/" className='hover:bg-purple-900 mb-2 inline-block'>&lt; Voltar</Link>
       {!!frontmatter?.headImage && !!frontmatter?.title &&
-      <img className='post-headimage' src={frontmatter.headImage} alt={frontmatter.title} />
+        <Image className='post-headimage' src={frontmatter.headImage} alt={frontmatter.title} />
       }
-      <h1>{ frontmatter.title }</h1>
+      <h1>{frontmatter.title}</h1>
       {!!frontmatter.headImage ?
-      <article dangerouslySetInnerHTML={{ __html: md().render(content) }} /> :
-      <p className='align-center text-gray-300 text-3xl text-center'>EM BREVE</p>
+        <article dangerouslySetInnerHTML={{ __html: md().render(content) }} /> :
+        <p className='align-center text-gray-300 text-3xl text-center'>EM BREVE</p>
       }
     </div>
   );
--- src/app/components/PostItem.tsx
+++ src/app/components/PostItem.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Link from "next/link";
+import Image from 'next/image';
 
 export interface PostItemProps {
   slug: string;
@@ -12,20 +13,20 @@ export interface PostItemProps {
 
 export function PostItem({ title, slug, thumbnail, publishDate, yearOfOccurrence }: PostItemProps) {
   return (
-    <Link className={"timeline-item " + (publishDate == "TBD" ? "disabled" : "" )} href="/case/[slug]" as={`/case/${slug}`}>
-    <div className="rounded m-3">
-      <div className="flex-1 p-4 inline-block align-middle">
-        <h2 className={"font-bold text-4xl mb-2 inline-block " + (publishDate == "TBD" ? "text-gray-700" : "text-gray-300" )}>{title} ({yearOfOccurrence})&nbsp;</h2>
-        <span className={"text-xl " + (publishDate == "TBD" ? "text-gray-700" : "text-gray-600" )}>
-          Publicado: {publishDate}&nbsp;
-        </span>
+    <Link className={"timeline-item " + (publishDate == "TBD" ? "disabled" : "")} href="/case/[slug]" as={`/case/${slug}`}>
+      <div className="rounded m-3">
+        <div className="flex-1 p-4 inline-block align-middle">
+          <h2 className={"font-bold text-4xl mb-2 inline-block " + (publishDate == "TBD" ? "text-gray-700" : "text-gray-300")}>{title} ({yearOfOccurrence})&nbsp;</h2>
+          <span className={"text-xl " + (publishDate == "TBD" ? "text-gray-700" : "text-gray-600")}>
+            Publicado: {publishDate}&nbsp;
+          </span>
+        </div>
+        <div className="w-48 h-48 inline-block align-middle ">
+          <span className="faded faded-all">
+            <Image className="h-48" src={thumbnail!} alt={title} />
+          </span>
+        </div>
       </div>
-      <div className="w-48 h-48 inline-block align-middle ">
-      <span className="faded faded-all">
-        <img className="h-48" src={thumbnail} alt={title} />
-        </span>
-      </div>
-    </div>
     </Link>
   );
 }
